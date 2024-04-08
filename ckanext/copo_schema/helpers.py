0b3b8e4f6c3f5a5c3e31e47dcfb5572397ceec07
debbyku <debbyku.2008@gmail.com>
2024-04-08 11:14:47 +0100

    update

--- ckanext/copo_schema/helpers.py
+++ ckanext/copo_schema/helpers.py
@@ -2,7 +2,7 @@
 from ckan.common import config
 
 def get_all_dataset_type():
-    datasets_str = config.get('ckan.all.dataset_type',"")
+    datasets_str = config.get('copo_schema.all_dataset_types',"")
     if datasets_str:
         return datasets_str.split(",")
     else:
